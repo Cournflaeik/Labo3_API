8af7464920c6bcb3d49a5b528cbac902dd2e6035
Cournflaeik <elien.degeetere@gmail.com>
2022-10-11 22:20:41 +0200

    fix: add comments for readability

--- src/weather.js
+++ src/weather.js
@@ -2,35 +2,43 @@ export default class Weather {
     constructor(api_key) {
         this.apiKey = api_key;
         if (
+            // check if weather data is stored in local storage
             localStorage.getItem('weather') && 
             Date.now() - localStorage.getItem('timestamp') < 600000
         ) {
+            // if it is, display it
             const weatherData = JSON.parse(localStorage.getItem('weather'));
             this.displayWeather(weatherData);
             console.log('weather data from local storage');
         }
         else {
-        this.getLocation();
+            // if not, get it from the API
+            this.getLocation();
         }
     }
     
     getLocation() {
         if (navigator.geolocation) {
+            // get the user's location
             navigator.geolocation.getCurrentPosition(this.getWeather.bind(this));
         } else {
+            // if the user doesn't allow it, display a message in the console
             console.log("Geolocation is not supported by this browser.");
         }
     }
 
     getWeather(position) {
+        // get the weather data from the API
         console.log(position);
         let lat = position.coords.latitude;
         let lon = position.coords.longitude;
+        // fetch the weather data from the API
         const url = `https://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${lat},${lon}`;
         fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            // store the weather data in local storage
             localStorage.setItem('weather', JSON.stringify(data));
             localStorage.setItem('timestamp', Date.now());
             this.displayWeather(data);
@@ -38,6 +46,7 @@ export default class Weather {
     }	
 
     displayWeather(data) {
+        // display the weather data
         const temp = data.current.temp_c;
         document.querySelector('.weather__temp').innerHTML = temp + '°C';
 
